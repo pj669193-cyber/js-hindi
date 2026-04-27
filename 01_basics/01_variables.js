67fe986442b948432803d22ebe395c41b7995725
pranav jadhav <pj669193@gmail.com>
2026-04-27 18:39:52 +0000

    added variables and conversion files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 134245
+let accountEmails = "pranavjadhav@344.com"
+var accountPassword = "28282"
+accountCity = "pune"
+let accountState;
+
+// accountId = 2 // not allowed u can't use const more than one
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
